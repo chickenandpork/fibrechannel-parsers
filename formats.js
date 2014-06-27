128da93ed6accfb417dc0f97ad803f4902590066
Allan Clark <allanc@chickenandporn.com>
2014-06-27 10:00:43 -0700

    corrected NicknameParser property usage

--- formats.js
+++ formats.js
@@ -1,9 +1,9 @@
 var formats =
 [
     [ "Brocade \"alishow\" and \"zoneshow\"", "AliShowZoneParser.html", null ],
-    [ "BNA Database", "BNAPURLConnection.html", null ],
-    [ "BNA Database", "BNAZoneParser.html", null ],
-    [ "DCNM Database", "DCNMURLConnection.html", null ],
+    [ "Brocade BNA Database", "BNAPURLConnection.html", null ],
+    [ "Brocade BNA Database", "BNAZoneParser.html", null ],
+    [ "Cisco DCNM Database", "DCNMURLConnection.html", null ],
     [ "Cisco \"show device-alias database\"", "DeviceAliasParser.html", null ],
     [ "NetApp OCI Database", "DWHURLConnection.html", null ],
     [ "NetApp OCI Database", "MSDWHURLConnection.html", null ],
